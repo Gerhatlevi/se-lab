0f6ac464de3394a458395f38a4d1c9ac11bba7fb
Gerhatlevi <glevi200312@gmail.com>
2023-09-20 17:43:19 +0200

    Revert "fndfs"
    This reverts commit c81dcb194733836538f5d0cfcfd04933bbd98e6e.

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -37,7 +37,11 @@ public class GT4500 implements SpaceShip {
   public boolean fireTorpedo(FiringMode firingMode) {
 
     boolean firingSuccess = true;
+<<<<<<< HEAD
     int lol = 1;
+=======
+    int lol = 1;
+>>>>>>> branch-B
 
     switch (firingMode) {
       case SINGLE:
